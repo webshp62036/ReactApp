b1dae5906c072c065204832fd80e7fdf51d41553
=webshp62036 <=subash.prasad@stackmybiz.com>
2022-03-28 19:08:19 +0530

    about & services page

--- src/components/Heros1.js
+++ src/components/Heros1.js
@@ -7,7 +7,9 @@ function Heros1(props){
             <div className="md:grid md:grid-cols-2 p-5  ">
             <div className=" items-center md:pl-5 ml-auto mr-auto mt-auto mb-auto md:w-1/2 ">
                   <h2 className="font-bold text-2xl -tracking-wide font-[Poppins]">{title}</h2><br/>
-                  <p className="font-[Poppins]">{content}</p>
+                  <p className="font-[Poppins]">{content}
+                  
+                </p>
               </div>
               <div className=" md:ml-32 md:p-10 md:w-1/2 p-2 mt-5">
                   <img src={image} alt="heros" width="400" height="auto"/>
